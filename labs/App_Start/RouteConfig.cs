1a698813daec8932e4cd994b36d0e098e45c50ef
hadarco <34810525+hadarco@users.noreply.github.com>
2019-07-09 20:03:11 +0300

    Add files via upload

---
+++ labs/App_Start/RouteConfig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace labs
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{resource}.config");
+
+
+         //   routes.MapRoute
+         //(
+         //name: "DefaultPage",
+         //url: "",
+         //defaults: new { controller = "Home", action = "Home", id = UrlParameter.Optional }
+         //);
+
+
+            //routes.MapRoute
+            //(
+            //name: "Add Product",
+            //url: "",
+            //defaults: new { controller = "Product", action = "Submit", id = UrlParameter.Optional }
+            //);
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+        }
+
+    }
+}
+
+
+
